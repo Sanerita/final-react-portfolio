ae9ff74bd13050723293301ad1030e652523a920
Sanelisiwe Sileku <sanelisiwe.sileku@gmail.com>
2023-09-18 13:09:30 +0200

    updated the links in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,13 +6,13 @@ const Footer = () => {
     <footer className="footer-container">
       <div className="my-footer">
       <div className="social-icons">
-        <a href="https://www.instagram.com/your-instagram-link" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.instagram.com/ndanellise/" target="_blank" rel="noopener noreferrer">
           <FaInstagram />
         </a>
-        <a href="https://github.com/your-github-link" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/Sanerita" target="_blank" rel="noopener noreferrer">
           <FaGithub />
         </a>
-        <a href="https://www.linkedin.com/in/your-linkedin-link" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/sanelisiwe-sileku-272b4b96/" target="_blank" rel="noopener noreferrer">
           <FaLinkedin />
         </a>
         <a href="https://twitter.com/your-twitter-link" target="_blank" rel="noopener noreferrer">
