f11ec444a3fb807d385eadcce15d48cb40159c02
Sanerita <sanelisiwe.sileku@gmail.com>
2023-11-09 15:14:47 +0200

    added demo buttons on projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,44 +5,44 @@ import projectB from '../assets/projectB.png';
 import Nezzies from '../assets/Nezzies.png';
 import calculator from '../assets/calculator.png';
 import { FaGithub } from 'react-icons/fa';
-// import '../mediaQueries.css';
+import { FaPlayCircle } from 'react-icons/fa';
+
 
 
 const Projects = () => {
 
   return (
-
-    <section id='Projects'>
+    <section id="Projects">
       <div className="projects-page">
-        <div className='project-title'>
-
+        <div className="project-title">
           <h2>PROJECTS</h2>
         </div>
 
-<div className='pro'>
-        <div className="projects-intro">
-          <h2>Welcome to My Portfolio of Diverse Projects</h2>
-          <p>
-            Explore a collection of my projects that showcase my journey in web development and
-            desktop applications. From crafting engaging HTML & CSS
-            websites to building dynamic React applications and sharing a Java application which I've done
-            during my university studies, my portfolio reflects my dedication to continuous learning and
-            problem-solving.
-          </p>
-          <p>
-            Whether you're interested in front-end web development or modern JavaScript applications,
-            you'll find a variety of projects that highlight my skills and
-            experiences.
-          </p>
-          <p>
-            Click on the projects to learn more, explore the source code on GitHub, and experience
-            live demos where available. If you have any questions, feedback, or potential collaborations
-            in mind, please feel free to get in touch. I'm excited to share my journey and discuss how
-            we can work together to create something remarkable.
-          </p>
+        <div className="pro">
+          <div className="projects-intro">
+            <h2>Welcome to My Portfolio of Diverse Projects</h2>
+            <p>
+              Explore a collection of my projects that showcase my journey in
+              web development and desktop applications. From crafting engaging
+              HTML & CSS websites to building dynamic React applications and
+              sharing a Java application which I've done during my university
+              studies, my portfolio reflects my dedication to continuous
+              learning and problem-solving.
+            </p>
+            <p>
+              Whether you're interested in front-end web development or modern
+              JavaScript applications, you'll find a variety of projects that
+              highlight my skills and experiences.
+            </p>
+            <p>
+              Click on the projects to learn more, explore the source code on
+              GitHub, and experience live demos where available. If you have any
+              questions, feedback, or potential collaborations in mind, please
+              feel free to get in touch. I'm excited to share my journey and
+              discuss how we can work together to create something remarkable.
+            </p>
+          </div>
         </div>
-        </div>
-
 
         <div className="container">
           <div className="box">
@@ -52,11 +52,34 @@ const Projects = () => {
             <div className="content">
               <div>
                 <h2>HTML&CSS Website</h2>
-                <p>This is a transport service business website 
-                  that is built with HTML5 and CSS and it is still under development</p>
-                <a href="https://github.com/Sanerita/Malimba-Website/tree/main/malimba" target="_blank" rel="noopener noreferrer">
-                  <button className='github-button'><FaGithub /> GitHub</button>
-                </a>
+                <p>
+                  This is a transport service business website that is built
+                  with HTML5 and CSS and it is still under development
+                </p>
+            
+                {/* ---------------------------------------- */}
+                <div className="side">
+                  <div className="articles">
+                    <a
+                      href="https://github.com/Sanerita/Malimba-Website/tree/main/malimba"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <button className="github-button">
+                        <FaGithub /> GitHub
+                      </button>
+                    </a>
+                  </div>
+                  <div className="articles">
+                    <a href="#" target="_blank" rel="noopener noreferrer">
+                      <button className="github-button">
+                        <FaPlayCircle /> Demo
+                      </button>
+                    </a>
+                  </div>
+                </div>
+
+                {/* ----------------------------------------- */}
               </div>
             </div>
           </div>
@@ -67,10 +90,19 @@ const Projects = () => {
             <div className="content">
               <div>
                 <h2> React Chat-App</h2>
-                <p>Allows users to engage in conversations with each other. It utilizes React for the
-                  front-end, Node.js for the server-side, and Chat Engine for real-time messaging functionality.</p>
-                <a href="https://github.com/Sanerita/CHAT-APP/tree/master/ChatBotWithReact-main" target="_blank" rel="noopener noreferrer">
-                  <button className='github-button'><FaGithub /> GitHub</button>
+                <p>
+                  Allows users to engage in conversations with each other. It
+                  utilizes React for the front-end, Node.js for the server-side,
+                  and Chat Engine for real-time messaging functionality.
+                </p>
+                <a
+                  href="https://github.com/Sanerita/CHAT-APP/tree/master/ChatBotWithReact-main"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="github-button">
+                    <FaGithub /> GitHub
+                  </button>
                 </a>
               </div>
             </div>
@@ -82,10 +114,19 @@ const Projects = () => {
             <div className="content">
               <div>
                 <h2>Finance Calculator</h2>
-                <p>Finance Calculator is a Python program that allows users to perform 
-                  financial calculations such as investment and bond calculations.</p>
-                <a href="https://github.com/Sanerita/Financial-calculator/tree/main/FinaceCalculator" target="_blank" rel="noopener noreferrer">
-                  <button className='github-button'><FaGithub /> GitHub</button>
+                <p>
+                  Finance Calculator is a Python program that allows users to
+                  perform financial calculations such as investment and bond
+                  calculations.
+                </p>
+                <a
+                  href="https://github.com/Sanerita/Financial-calculator/tree/main/FinaceCalculator"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="github-button">
+                    <FaGithub /> GitHub
+                  </button>
                 </a>
               </div>
             </div>
@@ -97,15 +138,34 @@ const Projects = () => {
             <div className="content">
               <div>
                 <h2>React Business website</h2>
-                <p>This is a local business website built with react.js and css</p>
-                <a href="https://github.com/Sanerita/Business-Website" target="_blank" rel="noopener noreferrer">
-                  <button className='github-button'><FaGithub /> GitHub</button>
-                </a>
+                <p>
+                  This is a local business website built with react.js and css
+                </p>
+
+                <div className="side">
+                  <div className="articles">
+                    <a
+                      href="https://github.com/Sanerita/Business-Website"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <button className="github-button">
+                        <FaGithub /> GitHub
+                      </button>
+                    </a>
+                  </div>
+                  <div className="articles">
+                    <a href="#" target="_blank" rel="noopener noreferrer">
+                      <button className="github-button">
+                        <FaPlayCircle /> Demo
+                      </button>
+                    </a>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-
       </div>
     </section>
   );
